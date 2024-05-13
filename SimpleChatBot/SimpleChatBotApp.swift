d1eb5c310c094a1922a76ec692904d5ba9c99c22
Mindy Douglas <mindy.douglas@icloud.com>
2024-05-13 16:22:02 -0500

    Initial Commit

---
+++ SimpleChatBot/SimpleChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatBotApp.swift
+//  SimpleChatBot
+//
+//  Created by Mindy Douglas on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
